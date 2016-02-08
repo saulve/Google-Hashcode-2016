c7307ff81a724791d91f7b31b7ffd68429e2530c
Nikita Patrikejev <cheloveq@Nikitas-MBP.default>
2016-02-08 22:03:39 +0000

    Added Brute Force and Kal's Method

--- src/Test.java
+++ src/Test.java
@@ -4,16 +4,8 @@ public class Test {
     public static void main(String[] args) {
         PictureGrid logo = new PictureGrid("input/logo.in");
 
-        logo.paintSquare(2, 2, 2);
-        logo.eraseCell(0,0);
-        logo.paintLine(2, 15, 2, 30);
-        logo.writeResults();
-        logo.printGrid();
-
-        for (int i=0; i<logo.getHeight(); i++){
-            for (int j=0; j<logo.getWidth(); j++){
-
-            }
-        }
+        //Algorithms.kalMethod(logo);
+        
+        Algorithms.bruteForce(logo);
     }
 }
